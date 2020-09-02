ed300e9fcbf50501a05205e680644ad3260285db
Dave Goldstein <dpg3k@virginia.edu>
2020-09-02 10:59:27 -0400

    Bring in packaging assets. Make a layered build. Update to latest Alpine

---
+++ package/scripts/entry.sh
@@ -0,0 +1,8 @@
+# run application
+
+# run from here, since application expects json template in templates/
+cd bin; ./aries-avalon -solrurl $ARIES_AVALON_SOLR_URL -solrcore $ARIES_AVALON_SOLR_CORE -avalonurl $ARIES_AVALON_URL
+
+#
+# end of file
+#
